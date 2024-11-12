63862d33412a3e38a2e275b8ac25d0ee3aef9271
Samir <samirmh15@gmail.com>
2024-11-12 22:47:23 +0100

    s

---
+++ config/database.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+let url = process.env.URI_MONGO
+
+console.log(url);
+
+async function ConnectDataBase() {
+    try {
+        await mongoose.connect(url)
+        console.log("Base de datos conectada");
+        
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+
+ConnectDataBase()
--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 import express from "express"
+import "dotenv/config.js"
+import "./config/database.js"
+
+const server = express()
+const PORT = process.env.PORT || 8080
+
+
+
+const ready = () => console.log("El servidor esta en el puerto " + PORT);
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
